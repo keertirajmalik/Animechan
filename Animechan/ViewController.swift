125bc4eff17888497be2964753cbf91051461ae3
keertirajmalik <keertirajmalik@gmail.com>
2022-04-21 17:39:21 +0530

    Initial Commit

---
+++ Animechan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animechan
+//
+//  Created by Keertiraj Laxman Malik on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
